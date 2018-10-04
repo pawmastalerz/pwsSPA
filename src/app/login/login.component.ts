0df02890cf9d1e7ffb9f8ade0935455299f0a3b0
Pawel <paw.mastalerz@gmail.com>
2018-10-04 14:36:19 +0200

    No unimportant code in login component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,29 +8,9 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  invalidLogin: boolean;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
-  login(form: NgForm) {
-    const credentials = JSON.stringify(form.value);
-    this.http
-      .post('http://localhost:5000/api/auth/login', credentials, {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      })
-      .subscribe(
-        response => {
-          const token = (<any>response).token;
-          localStorage.setItem('jwt', token);
-          this.invalidLogin = false;
-        },
-        err => {
-          this.invalidLogin = true;
-        }
-      );
-  }
 }
